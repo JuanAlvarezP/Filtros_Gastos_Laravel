7d9fea1a4df304b44c3dee9d007a3d23dce23131
Juan Alvarez <juan.alvarezpullas@gmail.com>
2024-12-26 17:35:06 -0500

    Modelos de datos

--- app/Models/Departamento.php
+++ app/Models/Departamento.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Departamento extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nombre'];
+
+    public function empleados()
+    {
+        return $this->hasMany(Empleado::class);
+    }
+
+    public function gastos()
+    {
+        return $this->hasMany(Gasto::class);
+    }
 }
--- app/Models/Gasto.php
+++ app/Models/Gasto.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Gasto extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['descripcion', 'monto', 'fecha', 'empleado_id', 'departamento_id'];
+
+    public function empleado()
+    {
+        return $this->belongsTo(Empleado::class);
+    }
+
+    public function departamento()
+    {
+        return $this->belongsTo(Departamento::class);
+    }
 }
